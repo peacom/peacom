22f7079def6c3b80530d1a82a642626956e5ab04
Le Phuoc Canh <lephuoccanh@gmail.com>
2022-12-28 15:54:49 +0700

    Update api

--- packages/core/src/lib/api/api.constant.ts
+++ packages/core/src/lib/api/api.constant.ts
@@ -1,5 +1,4 @@
-import {Application, PARTNER, SEND_MESSAGE_VIA} from '../model/';
-import {RawMessage} from "../model/";
+import {Application, PARTNER, RawMessage, SEND_MESSAGE_VIA} from '../model/';
 
 interface ApplicationInfo {
   application: Application
@@ -32,10 +31,24 @@ export interface ApiChannelMessageParams {
   application: Record<string, unknown>,
   partnerId: PARTNER,
   rawMessage: RawMessage,
-  messageId: string // message Public ID
+  messageId?: string // message Public ID
   receiverId: string
   sendVia: SEND_MESSAGE_VIA
   queueOption?: Record<string, unknown>
 }
 
 export type LOG_FUNCTION = ((t: unknown) => void) | null
+
+export const MICROSERVICE_URL: Record<Application, string> = {
+  [Application.APPLE]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4005',
+  [Application.ZALO]: process.env['ZALO_INTEGRATION'] || 'http://127.0.0.1:4001',
+  [Application.ZALO_ZNS]: process.env['ZALO_INTEGRATION'] || 'http://127.0.0.1:4001',
+  [Application.RCS]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4006',
+  [Application.VIBER]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4007',
+  [Application.VIBER_BUSINESS]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4008',
+  [Application.TELEGRAM]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4009',
+  [Application.FACEBOOK]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4010',
+  [Application.SMS]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4011',
+}
+
+export const INTEGRATED_CHANNEL = [Application.APPLE, Application.ZALO, Application.ZALO_ZNS]
