0c45e7345ebd6a223f3c4bec21b3667271158664
Le Phuoc Canh <lephuoccanh@gmail.com>
2023-01-09 20:43:45 +0700

    Update QueueMessageStatus

--- packages/core/src/lib/model/queue/QueueMessage.ts
+++ packages/core/src/lib/model/queue/QueueMessage.ts
@@ -1,15 +1,22 @@
 import {Application} from "../";
 import {MESSAGE_STATUS} from "../Message";
+import {JobsOptions} from "bullmq";
 
+export enum QueueMessageStatusSource {
+  LOCAL = 1,
+  PARTNER = 2
+}
 
 export interface QueueMessageStatus {
+  messageId: string;
   applicationMessageId: string;
   applicationId: Application;
   status: MESSAGE_STATUS;
   date: number;
   extraData: Record<string, unknown>;
   deliveryStatus?: string,
-  jobOpt: Record<string, unknown>
+  jobOpt?: JobsOptions,
+  source: QueueMessageStatusSource
 }
 
 
