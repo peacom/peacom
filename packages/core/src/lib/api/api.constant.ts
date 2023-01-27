06174a6b6289537664061dfce8c7c8f745413836
Le Phuoc Canh <lephuoccanh@gmail.com>
2023-01-27 16:58:00 +0700

    Update for Viber Business Params.

--- packages/core/src/lib/api/api.constant.ts
+++ packages/core/src/lib/api/api.constant.ts
@@ -42,21 +42,22 @@ export interface ApiChannelMessageParams {
   messageId?: string // message Public ID
   receiverId: string
   sendVia: SEND_MESSAGE_VIA
-  queueOption?: Record<string, unknown>
+  queueOption?: Record<string, unknown>,
+  [key: string]: unknown
 }
 
 export type LOG_FUNCTION = ((t: unknown) => void) | null
 
 export const MICROSERVICE_URL: Record<Application, string> = {
   [Application.APPLE]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4005/eip/apple',
-  [Application.ZALO]: process.env['ZALO_INTEGRATION'] || 'http://127.0.0.1:4001/eip/zalo',
-  [Application.ZALO_ZNS]: process.env['ZALO_INTEGRATION'] || 'http://127.0.0.1:4001/eip/zalo',
-  [Application.RCS]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4006',
-  [Application.VIBER]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4007',
-  [Application.VIBER_BUSINESS]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4008',
-  [Application.TELEGRAM]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4009',
-  [Application.FACEBOOK]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4010',
-  [Application.SMS]: process.env['APPLE_INTEGRATION'] || 'http://127.0.0.1:4011',
+  [Application.ZALO]: process.env['ZALO_INTEGRATION'] || 'http://127.0.0.1:4001/eip/zalo/bot',
+  [Application.ZALO_ZNS]: process.env['ZALO_ZNS_INTEGRATION'] || 'http://127.0.0.1:4001/eip/zalo/zns',
+  [Application.RCS]: process.env['RCS_INTEGRATION'] || 'http://127.0.0.1:4006',
+  [Application.VIBER]: process.env['VIBER_BOT_INTEGRATION'] || 'http://127.0.0.1:4001/eip/viber/bot',
+  [Application.VIBER_BUSINESS]: process.env['VIBER_BUSINESS_INTEGRATION'] || 'http://127.0.0.1:4001/eip/viber/business',
+  [Application.TELEGRAM]: process.env['TELEGRAM_INTEGRATION'] || 'http://127.0.0.1:4009',
+  [Application.FACEBOOK]: process.env['FACEBOOK_INTEGRATION'] || 'http://127.0.0.1:4010',
+  [Application.SMS]: process.env['SMS_INTEGRATION'] || 'http://127.0.0.1:4011',
 }
 
 export const INTEGRATED_CHANNEL = [Application.APPLE, Application.ZALO, Application.ZALO_ZNS]
