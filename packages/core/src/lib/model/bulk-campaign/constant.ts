5081a3603ed108064a9c84a6d5ee20784b653c77
Le Phuoc Canh <lephuoccanh@gmail.com>
2023-06-19 14:02:47 +0700

    Add Download Large File

---
+++ packages/core/src/lib/model/bulk-campaign/constant.ts
@@ -0,0 +1,17 @@
+/**
+ * Cycle
+ * PENDING ==> OPTIMIZING => OPTIMIZED ==> STARTED ==> PROCESSING ==> DONE
+ *  - only allow update bulk campaign when STATUS in (PENDING, OPTIMIZE, DONE)
+ *    1. Any change relative to channel ==> Not allow
+ *    2. Other change is ok.
+ *  - only broadcast BULK CAMPAIGN WHEN STATUS OPTIMIZE
+ * @type {{DONE: number, OPTIMIZE: number, STARTED: number, PROCESSING: number, PENDING: number}}
+ */
+export enum BULK_CAMPAIGN_RUN_STATUS {
+  PENDING = 1,
+  STARTED,
+  PROCESSING,
+  DONE,
+  OPTIMIZING,
+  OPTIMIZED
+}
--- packages/core/src/lib/service/storage/aws/s3.spec.ts
+++ packages/core/src/lib/service/storage/aws/s3.spec.ts
@@ -1,4 +1,5 @@
-import {downloadS3Url, uploadS3FromUrl} from "./s3.service";
+import {downloadS3Url, uploadLocalFileToS3, uploadS3FromUrl} from "./s3.service";
+import * as path from "path";
 
 describe('s3.service.specs.ts', () => {
   it('uploadS3FromUrl test env key', async () => {
@@ -9,6 +10,11 @@ describe('s3.service.specs.ts', () => {
       name: 'birds_PNG9.png', type: 'image/png', extension: 'png',
       "url": "https://ap-southeast-1-dev-peacom.s3.ap-southeast-1.amazonaws.com/files/birds_PNG9.png"
     });
+  }, 200000);
+  it('uploadS3FromLocalFile xml', async () => {
+    const xmlFile = path.resolve( 'src', 'lib', 'service', 'storage', 'aws', 'RBM_7_10_data.xlsx')
+    const rs = await uploadLocalFileToS3({filePath: xmlFile})
+    console.log(rs);
   }, 200000);
   it('Download S3 Large file', async () => {
     await downloadS3Url({
