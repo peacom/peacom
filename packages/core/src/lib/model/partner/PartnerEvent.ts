4f8d93cced0bec6b71717d961b5791392a6ad39d
Le Phuoc Canh <lephuoccanh@gmail.com>
2023-07-21 10:47:41 +0700

    Update description of PartnerEvent.ts

--- packages/core/src/lib/model/partner/PartnerEvent.ts
+++ packages/core/src/lib/model/partner/PartnerEvent.ts
@@ -4,13 +4,13 @@ import {JobsOptions} from "bullmq";
 import {Application} from "../Application";
 
 export interface PartnerEvent {
-  messageId: string
+  messageId: string // Peacom Message Public ID = Parnter Application Message ID
   type: PARTNER_MESSAGE_TYPE
   timestamp: number
   cause: string
   status: MESSAGE_STATUS
   message?: RawMessage
-  publicId?: string // channel publicId
+  publicId?: string // channel publicId OA, BOT Public ID
   applicationId: Application
   from?: string
   clientMessageId?: string
