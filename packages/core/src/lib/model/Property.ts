3b8ff840a3134d8fa9e45e2ce9703d031ebd7f3c
tanduy4899 <tanduy899@gmail.com>
2023-08-25 21:16:35 +0700

    - update crm

--- packages/core/src/lib/model/Property.ts
+++ packages/core/src/lib/model/Property.ts
@@ -8,7 +8,9 @@ export enum TYPE_PROPERTY {
   NUMBER = 1,
   TEXT = 2,
   DATE = 3,
-  SELECT = 4
+  SELECT = 4,
+  AGE = 5,
+  GENDER = 6
 }
 
 export const typePropertyStr = (id: TYPE_PROPERTY | null) => {
@@ -21,7 +23,38 @@ export const typePropertyStr = (id: TYPE_PROPERTY | null) => {
       return "DATE"
     case TYPE_PROPERTY.SELECT:
       return "SELECT"
+    case TYPE_PROPERTY.AGE:
+      return "AGE"
+    case TYPE_PROPERTY.GENDER:
+      return "GENDER"
     default:
       return ""
   }
+}
+
+export enum CONDITION {
+  AND = "1",
+  OR = "2",
+}
+
+export enum SegmentType {
+  GROUP,
+  FIELD,
+}
+
+export enum PROPERTY_OPERATION {
+  IS_EQUAL,
+  IS_NOT_EQUAL,
+  CONTAINS,
+  DOES_NOT_CONTAIN,
+  BEGINS_WITH,
+  DOES_NOT_BEGIN_WITH,
+  ENDS_WITH,
+  DOES_NOT_AND_WITH,
+  NO_VALUE_SET,
+  VALUE_IS_SET,
+  IS_LESS,
+  IS_LESS_OR_EQUAL,
+  IS_GREATER,
+  IS_GREATER_OR_EQUAL
 }
