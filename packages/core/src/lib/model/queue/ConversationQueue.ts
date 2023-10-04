230672a0ff2a1cfb7395d70c6d5063fbd5b2757a
tanduy4899 <tanduy899@gmail.com>
2023-10-04 12:04:25 +0700

    CONVERSATION_QUEUE

---
+++ packages/core/src/lib/model/queue/ConversationQueue.ts
@@ -0,0 +1,19 @@
+/**
+ * Define QUEUE by partner and service, for guarantee the limit rate
+ */
+export enum CONVERSATION_QUEUE {
+  TELEGRAM_IN = "TELEGRAM_IN",
+  TELEGRAM_OUT = "TELEGRAM_OUT",
+  VIBER_IN = "VIBER_IN",
+  VIBER_OUT = "VIBER_OUT",
+  RCS_IN = "RCS_IN",
+  RCS_OUT = "RCS_OUT",
+  APPLE_IN = "APPLE_IN",
+  APPLE_OUT = "APPLE_OUT",
+  FACEBOOK_IN = "FACEBOOK_IN",
+  FACEBOOK_OUT = "FACEBOOK_OUT",
+  ZALO_OA_IN = "ZALO_OA_IN",
+  ZALO_OA_OUT = "ZALO_OA_OUT",
+  WHATSAPP_IN = "WHATSAPP_IN",
+  WHATSAPP_OUT = "WHATSAPP_OUT",
+}
