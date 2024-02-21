9578c53f4f608b8aadcc8b04e3a5a07d8c0e6818
Le Phuoc Canh <lephuoccanh@gmail.com>
2024-02-21 22:15:17 +0700

    DEFINE Again VIBER Integration

--- packages/core/src/lib/api/api.constant.ts
+++ packages/core/src/lib/api/api.constant.ts
@@ -84,7 +84,7 @@ export const MICROSERVICE_URL: Record<Application, string> = {
   [Application.ZALO]: process.env['ZALO_INTEGRATION'] || 'http://127.0.0.1:4003/eip/zalo/bot',
   [Application.ZALO_ZNS]: process.env['ZALO_ZNS_INTEGRATION'] || 'http://127.0.0.1:4003/eip/zalo/zns',
   [Application.RCS]: process.env['RCS_INTEGRATION'] || 'http://127.0.0.1:4006/eip/google/rcs',
-  [Application.VIBER]: process.env['VIBER_BOT_INTEGRATION'] || 'http://127.0.0.1:4001/eip/viber/bot',
+  [Application.VIBER]: process.env['VIBER_BUSINESS_INTEGRATION'] || 'http://127.0.0.1:4001/eip/viber/business',
   [Application.VIBER_BUSINESS]: process.env['VIBER_BUSINESS_INTEGRATION'] || 'http://127.0.0.1:4001/eip/viber/business',
   [Application.TELEGRAM]: process.env['TELEGRAM_INTEGRATION'] || 'http://127.0.0.1:4009',
   [Application.FACEBOOK]: process.env['FACEBOOK_INTEGRATION'] || 'http://127.0.0.1:4010',
