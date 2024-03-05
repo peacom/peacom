59eab5ad3c41e49bb205d56a9ce0ec58a4a7cb83
Le Phuoc Canh <lephuoccanh@gmail.com>
2024-03-05 10:25:36 +0700

    - model  - core => model

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
 // export model from frontend
-// export * from '@peacom/model'
+export * from '@peacom/model'
 // model for backend
-// export * from './lib/model'
+export * from './lib/model'
 // Util from backend
-// export * from './lib/util'
+export * from './lib/util'
 export * from './lib/api'
 export * from './lib/service'
