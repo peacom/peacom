2cce02eb01be4b970812c48c26eb542ab76af895
levanchien <levanchien95.cit@gmail.com>
2024-03-25 20:26:22 +0700

    Add notification model

---
+++ packages/model/src/lib/model/Notification.ts
@@ -0,0 +1,10 @@
+export enum NotificationPriority {
+  HIGH = 1,
+  NORMAL = 2,
+}
+
+export enum MessageEvent {
+  CHANNEL_OA_STATUS = 'channel.oa.status',
+  CHANNEL_REQUEST = 'channel.request',
+  TEMPLATE_REQUEST = 'template.request',
+}
