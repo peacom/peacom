014d74106b5df4cdf6d1f3f5150250c94d54306f
levanchien <levanchien95.cit@gmail.com>
2024-04-20 10:44:43 +0700

    Add new MessageEvent type

--- packages/model/src/lib/model/Notification.ts
+++ packages/model/src/lib/model/Notification.ts
@@ -7,4 +7,7 @@ export enum MessageEvent {
   CHANNEL_OA_STATUS = 'channel.oa.status',
   CHANNEL_REQUEST = 'channel.request',
   TEMPLATE_REQUEST = 'template.request',
+  COMPANY_KYC_REQUEST = 'company.kyc.request',
+  COMPANY_KYC_REJECT = 'company.kyc.reject',
+  COMPANY_KYC_APPROVE = 'company.kyc.approve',
 }
