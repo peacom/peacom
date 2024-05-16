5d5e4e6009de3bb851a5b97b4f1f411cc9fd4807
levanchien <levanchien95.cit@gmail.com>
2024-05-16 11:51:04 +0700

    Add more message event

--- packages/model/src/lib/model/Notification.ts
+++ packages/model/src/lib/model/Notification.ts
@@ -7,6 +7,8 @@ export enum MessageEvent {
   CHANNEL_OA_STATUS = 'channel.oa.status',
   CHANNEL_REQUEST = 'channel.request',
   TEMPLATE_REQUEST = 'template.request',
+  TEMPLATE_APPROVE = 'template.approve',
+  TEMPLATE_REJECT = 'template.reject',
   COMPANY_KYC_REQUEST = 'company.kyc.request',
   COMPANY_KYC_REJECT = 'company.kyc.reject',
   COMPANY_KYC_APPROVE = 'company.kyc.approve',
