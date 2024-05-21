b295bdc5a2d523459c62fafdcebf85dfb6986770
Le Phuoc Canh <lephuoccanh@gmail.com>
2024-05-21 15:15:17 +0700

    Update test string.

--- packages/model/src/lib/model/acl/CompanyPermission.ts
+++ packages/model/src/lib/model/acl/CompanyPermission.ts
@@ -156,6 +156,5 @@ export const LIST_MASTER_COMPANY_PERMISSION = [
   /* CHAT */
   { permission: PERMISSION.CHAT.READ, type: AclActionType.FULL },
   { permission: PERMISSION.CHAT.LIVE_AGENT, type: AclActionType.FULL },
-  { permission: PERMISSION.CHAT.DASHBOARD, type: AclActionType.FULL },
-  { permission: PERMISSION.CHANNEL.REQUEST, type: AclActionType.FULL },
+  { permission: PERMISSION.CHAT.DASHBOARD, type: AclActionType.FULL }
 ];
