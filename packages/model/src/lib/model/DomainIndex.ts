4775717f1bee029224d8f045d6423fc481e8890c
levanchien <levanchien95.cit@gmail.com>
2024-06-14 15:12:28 +0700

    Add interface SearchDomainQuery

--- packages/model/src/lib/model/DomainIndex.ts
+++ packages/model/src/lib/model/DomainIndex.ts
@@ -10,3 +10,13 @@ export enum DomainIndexAction {
   UPDATE = 'UPDATE',
   DELETE = 'DELETE',
 }
+
+export interface SearchDomainQuery {
+  search: string;
+  page: number; // start from 0
+  size: number;
+  sort: string[]; // [field:asc|desc]
+  filters: {
+    [field: string]: string | number | object; // if value = object then use or operator
+  };
+}
