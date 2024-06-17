f669f86523a7fd0a5b2ed1d50ec6bfd817ca59c8
levanchien <levanchien95.cit@gmail.com>
2024-06-17 13:34:31 +0700

    Update domain index search query

--- packages/model/src/lib/model/DomainIndex.ts
+++ packages/model/src/lib/model/DomainIndex.ts
@@ -11,12 +11,28 @@ export enum DomainIndexAction {
   DELETE = 'DELETE',
 }
 
+export enum SearchOP {
+  or = 'or',
+  and = 'and',
+  gt = 'gt',
+  gte = 'gte',
+  lt = 'lt',
+  lte = 'lte',
+  range = 'range',
+  not = 'not',
+}
+
+export interface SearchObject {
+  value: string | number | Array<string | number>;
+  op: SearchOP;
+}
+
 export interface SearchDomainQuery {
   search: string;
   page: number; // start from 0
   size: number;
-  sort: string[]; // [field:asc|desc]
+  sorts: string[]; // [field:asc|desc]
   filters: {
-    [field: string]: string | number | object; // if value = object then use or operator
+    [field: string]: string | number | SearchObject;
   };
 }
