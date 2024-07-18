44f7df2a56de88eb993bdc1c4265e9a6dfbadb92
duynt <tanduy899@gmail.com>
2024-07-18 16:26:16 +0700

    - update http error

--- packages/core/src/lib/service/storage/aws/s3.spec.ts
+++ packages/core/src/lib/service/storage/aws/s3.spec.ts
@@ -1,48 +1,51 @@
 import {
   createPreSignedUrl,
   downloadS3Url,
-  getAwsKeyInfo, getS3UrlKey,
+  getAwsKeyInfo, getAwsUrlInfo, getS3UrlKey,
   s3RemoveFile, s3RemoveMultipleFile,
   uploadLocalFileToS3,
   uploadS3FromUrl
-} from "./s3.service";
-import * as path from "path";
+} from './s3.service';
+import * as path from 'path';
 
 describe('s3.service.specs.ts', () => {
   it('uploadS3FromUrl test env key', async () => {
-    console.log(process.env['S3_SECRET_KEY'])
-    const rs = await uploadS3FromUrl({url: 'https://pngimg.com/uploads/birds/birds_PNG9.png'})
+    console.log(process.env['S3_SECRET_KEY']);
+    const rs = await uploadS3FromUrl({ url: 'https://pngimg.com/uploads/birds/birds_PNG9.png' });
     console.log(rs);
     expect(rs).toEqual({
       name: 'birds_PNG9.png', type: 'image/png', extension: 'png',
-      "key": "files/birds_PNG9.png", "size": 827214,
-      "url": "https://ap-southeast-1-dev-peacom.s3.ap-southeast-1.amazonaws.com/files/birds_PNG9.png"
+      'key': 'files/birds_PNG9.png', 'size': 827214,
+      'url': 'https://ap-southeast-1-dev-peacom.s3.ap-southeast-1.amazonaws.com/files/birds_PNG9.png'
     });
   }, 200000);
   it('uploadS3FromLocalFile xml', async () => {
-    const xmlFile = path.resolve('src', 'lib', 'service', 'storage', 'aws', 'RBM_7_10_data.xlsx')
-    const rs = await uploadLocalFileToS3({filePath: xmlFile})
+    const xmlFile = path.resolve('src', 'lib', 'service', 'storage', 'aws', 'RBM_7_10_data.xlsx');
+    const rs = await uploadLocalFileToS3({ filePath: xmlFile });
     console.log(rs);
   }, 200000);
   it('Download S3 Large file', async () => {
     await downloadS3Url({
       url: 'https://ap-southeast-1-dev-peacom.s3.ap-southeast-1.amazonaws.com/files/contactimportsamplefullxlsx.xlsx',
       outputFile: './contactimportsamplefullxlsx.xlsx', chunkSize: 1024 * 1024
-    })
-  }, 200000)
+    });
+  }, 200000);
   it('createPreSignedUrl', async () => {
-    console.log(await createPreSignedUrl({fileName: 'test1234', contentType: 'application/xml'}))
-  })
+    console.log(await createPreSignedUrl({ fileName: 'test1234', contentType: 'application/xml' }));
+  });
   it('getS3UrlKey', async () => {
-    console.log(getS3UrlKey("https://dev-cdn.peacom.co/files/tes.png"))
-  })
+    console.log(getS3UrlKey('https://dev-cdn.peacom.co/files/tes.png'));
+  });
   it('Get S3 File', async () => {
-    console.log(await getAwsKeyInfo('files/DaNangNHxlsx_e45afd6a-47c6-42ae-af7b-21f567a96a3c.xlsx'))
-  })
+    console.log(await getAwsKeyInfo('files/DaNangNHxlsx_e45afd6a-47c6-42ae-af7b-21f567a96a3c.xlsx'));
+  });
   it('Delete S3 File', async () => {
-    console.log(await s3RemoveFile({key: 'files/RBM_7_10_data.xlsx'}))
-  })
+    console.log(await s3RemoveFile({ key: 'files/RBM_7_10_data.xlsx' }));
+  });
   it('Delete Multiple S3 File', async () => {
-    console.log(await s3RemoveMultipleFile({keys: ['files/birds_PNG9.png', '1234']}))
-  })
-})
+    console.log(await s3RemoveMultipleFile({ keys: ['files/birds_PNG9.png', '1234'] }));
+  });
+  it('Get S3 URL info', async () => {
+    console.log(await getAwsUrlInfo('https://ap-southeast-1-dev-peacom.s3.ap-southeast-1.amazonaws.com/files/contactimportsamplefullxlsx.xlsx'));
+  });
+});
