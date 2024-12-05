ea9fdef09d1f482f2cd5ef6f94281df37206eb4d
duynt <tanduy899@gmail.com>
2024-12-05 16:42:13 +0700

    - update lib

--- packages/core/src/lib/service/storage/aws/constants.ts
+++ packages/core/src/lib/service/storage/aws/constants.ts
@@ -2,15 +2,15 @@
 import { S3 } from "@aws-sdk/client-s3"
 
 export const S3_OPTION = {
-  accessKeyId: process.env['S3_ACCESS_KEY'] || 'AKIATVLTGO26C3NWH2HR',
-  secretAccessKey: process.env['S3_SECRET_KEY'] || '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',
-  endpoint: process.env['S3_ENDPOINT'] || 'https://s3.ap-southeast-1.amazonaws.com',
-  region: process.env['S3_REGION'] || 'ap-southeast-1',
+  accessKeyId: process.env['S3_ACCESS_KEY'],
+  secretAccessKey: process.env['S3_SECRET_KEY'],
+  endpoint: process.env['S3_ENDPOINT'],
+  region: process.env['S3_REGION'],
   domain: process.env['S3_DOMAIN'] || ''
 };
 
 export const S3_INFO = {
-  BUCKET: process.env['S3_BUCKET'] || 'ap-southeast-1-dev-peacom'
+  BUCKET: process.env['S3_BUCKET']
 }
 
 export const S3_FOLDERS = {
