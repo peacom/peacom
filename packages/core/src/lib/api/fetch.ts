05798b8487c590e9aad40cd232d51d9eae2bcc6e
Le Phuoc Canh <lephuoccanh@gmail.com>
2024-12-12 01:43:44 +0700

    Add function for parse response

--- packages/core/src/lib/api/fetch.ts
+++ packages/core/src/lib/api/fetch.ts
@@ -8,3 +8,26 @@ export const myFetch = (url: any, opts?: any) => {
   }
   return fetch(url, options);
 };
+
+export const parseResponse = async (resp: Response, log?: (message: string) => void) => {
+  const responseBody = await resp.json();
+  if (log) {
+    log(
+      `Response ${resp.status} - ${resp.statusText}\n${JSON.stringify(
+        responseBody
+      )}`
+    );
+  }
+
+  if (!resp.ok) {
+    const error = responseBody[0];
+    let errorMessage = resp.statusText;
+    if (error) {
+      errorMessage = `${error.code} - ${error.message}`;
+    }
+
+    throw new Error(errorMessage);
+  }
+
+  return responseBody;
+}
