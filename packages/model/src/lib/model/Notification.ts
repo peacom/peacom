12edf45e879a9c4f71db8175070be6f8ed5a9712
lv chien <levanchien95.cit@gmail.com>
2025-03-18 17:31:43 +0700

    Update Notification

--- packages/model/src/lib/model/Notification.ts
+++ packages/model/src/lib/model/Notification.ts
@@ -1,4 +1,4 @@
-import {AclActionType} from "./acl";
+import { AclActionType } from './acl';
 
 export enum NotificationPriority {
   HIGH = 1,
@@ -11,37 +11,38 @@ export enum MessageEvent {
   TEMPLATE_REQUEST = 'template.request',
   TEMPLATE_APPROVE = 'template.approve',
   TEMPLATE_REJECT = 'template.reject',
+  TEMPLATE_DELETE = 'template.delete',
   COMPANY_KYC_REQUEST = 'company.kyc.request',
   COMPANY_KYC_REJECT = 'company.kyc.reject',
   COMPANY_KYC_APPROVE = 'company.kyc.approve',
   COMPANY_SUSPEND = 'company.suspend',
   COMPANY_SUSPEND_BO_SA = 'company.suspend.BO_SA',
   BACKGROUND_TASK = 'background.task',
-  BALANCE_ALERT = "balance.alert",
-  BULK_DONE = "bulk.done",
-  BULK_FINISHED = "bulk.finished",
-  WEBHOOK = "webhook",
-  VNA_SUMMARY_CHECK_IN_REPORT = "VNA_SUMMARY_CHECK_IN_REPORT"
+  BALANCE_ALERT = 'balance.alert',
+  BULK_DONE = 'bulk.done',
+  BULK_FINISHED = 'bulk.finished',
+  WEBHOOK = 'webhook',
+  VNA_SUMMARY_CHECK_IN_REPORT = 'VNA_SUMMARY_CHECK_IN_REPORT',
 }
 
 export interface ReceiverPermission {
-  permission: number,
-  aclType: AclActionType,
-  userId?: number // In case of owner, we will send to userId, no need to query from AclGroupAction
-  companyId?: number // In case of PARTIAL, MASTER, we will base on companyId, BO
+  permission: number;
+  aclType: AclActionType;
+  userId?: number; // In case of owner, we will send to userId, no need to query from AclGroupAction
+  companyId?: number; // In case of PARTIAL, MASTER, we will base on companyId, BO
 }
 
 export enum NotificationChannel {
-  WEB = "WEB",
-  EMAIL = "EMAIL",
-  SMS = "SMS"
+  WEB = 'WEB',
+  EMAIL = 'EMAIL',
+  SMS = 'SMS',
 }
 
 export interface NotificationMessage {
-  message: MessageEvent,
-  priority: NotificationPriority,
-  excludeUser?: Array<number>,
-  params?: any,
-  receiver: Array<ReceiverPermission>
-  channels?: Array<NotificationChannel>
+  message: MessageEvent;
+  priority: NotificationPriority;
+  excludeUser?: Array<number>;
+  params?: any;
+  receiver: Array<ReceiverPermission>;
+  channels?: Array<NotificationChannel>;
 }
