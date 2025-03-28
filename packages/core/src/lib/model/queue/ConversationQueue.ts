03b913f2c77f1f5460232038a47c921e7aa7265f
lv chien <levanchien95.cit@gmail.com>
2025-03-28 16:46:00 +0700

    Add queue for Instagram

--- packages/core/src/lib/model/queue/ConversationQueue.ts
+++ packages/core/src/lib/model/queue/ConversationQueue.ts
@@ -1,49 +1,54 @@
-import {Application, applicationStr, PARTNER} from "@peacom/model";
+import { Application, applicationStr, PARTNER } from '@peacom/model';
 
 /**
  * Define QUEUE by partner and service, for guarantee the limit rate
  */
 export enum CONVERSATION_QUEUE {
-  TELEGRAM_IN = "TELEGRAM_IN",
-  TELEGRAM_OUT = "TELEGRAM_OUT",
-  VIBER_IN = "VIBER_IN",
-  VIBER_OUT = "VIBER_OUT",
-  RCS_IN = "RCS_IN",
-  RCS_OUT = "RCS_OUT",
-  RCS_DOTGO_IN = "RCS_DOTGO_IN",
-  RCS_DOTGO_OUT = "RCS_DOTGO_OUT",
-  APPLE_IN = "APPLE_IN",
-  APPLE_OUT = "APPLE_OUT",
-  FACEBOOK_IN = "FACEBOOK_IN",
-  FACEBOOK_OUT = "FACEBOOK_OUT",
-  ZALO_OA_IN = "ZALO_OA_IN",
-  ZALO_OA_OUT = "ZALO_OA_OUT",
-  WHATSAPP_IN = "WHATSAPP_IN",
-  WHATSAPP_OUT = "WHATSAPP_OUT",
-  GOOGLE_BUSINESS_IN = "GOOGLE_BUSINESS_IN",
-  GOOGLE_BUSINESS_OUT = "GOOGLE_BUSINESS_OUT",
-  PEACOM_IN = "PEACOM_IN",
-  PEACOM_OUT = "PEACOM_OUT",
-  LIVE_CHAT_IN = "LIVE_CHAT_IN",
-  LIVE_CHAT_OUT = "LIVE_CHAT_OUT",
+  TELEGRAM_IN = 'TELEGRAM_IN',
+  TELEGRAM_OUT = 'TELEGRAM_OUT',
+  VIBER_IN = 'VIBER_IN',
+  VIBER_OUT = 'VIBER_OUT',
+  RCS_IN = 'RCS_IN',
+  RCS_OUT = 'RCS_OUT',
+  RCS_DOTGO_IN = 'RCS_DOTGO_IN',
+  RCS_DOTGO_OUT = 'RCS_DOTGO_OUT',
+  APPLE_IN = 'APPLE_IN',
+  APPLE_OUT = 'APPLE_OUT',
+  FACEBOOK_IN = 'FACEBOOK_IN',
+  FACEBOOK_OUT = 'FACEBOOK_OUT',
+  ZALO_OA_IN = 'ZALO_OA_IN',
+  ZALO_OA_OUT = 'ZALO_OA_OUT',
+  WHATSAPP_IN = 'WHATSAPP_IN',
+  WHATSAPP_OUT = 'WHATSAPP_OUT',
+  GOOGLE_BUSINESS_IN = 'GOOGLE_BUSINESS_IN',
+  GOOGLE_BUSINESS_OUT = 'GOOGLE_BUSINESS_OUT',
+  PEACOM_IN = 'PEACOM_IN',
+  PEACOM_OUT = 'PEACOM_OUT',
+  LIVE_CHAT_IN = 'LIVE_CHAT_IN',
+  LIVE_CHAT_OUT = 'LIVE_CHAT_OUT',
+  INSTAGRAM_IN = 'INSTAGRAM_IN',
+  INSTAGRAM_OUT = 'INSTAGRAM_OUT',
 }
 
-export const getApplicationConversationQueueOutName = (applicationId: Application, partnerId: PARTNER | null = null) => {
+export const getApplicationConversationQueueOutName = (
+  applicationId: Application,
+  partnerId: PARTNER | null = null
+) => {
   if (partnerId === PARTNER.PEACOM) {
-    return CONVERSATION_QUEUE.PEACOM_OUT
+    return CONVERSATION_QUEUE.PEACOM_OUT;
   }
   switch (applicationId) {
     case Application.APPLE:
-      return CONVERSATION_QUEUE.APPLE_OUT
+      return CONVERSATION_QUEUE.APPLE_OUT;
     case Application.RCS:
       if (PARTNER.DOTGO === Number(partnerId)) {
-        return CONVERSATION_QUEUE.RCS_DOTGO_OUT
+        return CONVERSATION_QUEUE.RCS_DOTGO_OUT;
       }
-      return CONVERSATION_QUEUE.RCS_OUT
+      return CONVERSATION_QUEUE.RCS_OUT;
     case Application.WHATSAPP:
-      return CONVERSATION_QUEUE.WHATSAPP_OUT
+      return CONVERSATION_QUEUE.WHATSAPP_OUT;
     case Application.VIBER_BOT:
-      return CONVERSATION_QUEUE.VIBER_OUT
+      return CONVERSATION_QUEUE.VIBER_OUT;
     case Application.FACEBOOK:
       return CONVERSATION_QUEUE.FACEBOOK_OUT;
     case Application.GOOGLE_BUSINESS:
@@ -51,10 +56,16 @@ export const getApplicationConversationQueueOutName = (applicationId: Applicatio
     case Application.TELEGRAM:
       return CONVERSATION_QUEUE.TELEGRAM_OUT;
     case Application.ZALO:
-      return CONVERSATION_QUEUE.ZALO_OA_OUT
+      return CONVERSATION_QUEUE.ZALO_OA_OUT;
     case Application.LIVE_CHAT:
-      return CONVERSATION_QUEUE.LIVE_CHAT_OUT
+      return CONVERSATION_QUEUE.LIVE_CHAT_OUT;
+    case Application.INSTAGRAM:
+      return CONVERSATION_QUEUE.INSTAGRAM_OUT;
     default:
-      throw new Error(`Not support conversation for application ${applicationStr(applicationId)}`)
+      throw new Error(
+        `Not support conversation for application ${applicationStr(
+          applicationId
+        )}`
+      );
   }
-}
+};
