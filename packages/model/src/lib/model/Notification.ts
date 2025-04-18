161672253ab6ce6f66d2f261233b5c12906c6134
Le Phuoc Canh <lephuoccanh@gmail.com>
2025-04-18 23:36:57 +0700

    Add mapping MessageEvent to NotificationType

--- packages/model/src/lib/model/Notification.ts
+++ packages/model/src/lib/model/Notification.ts
@@ -1,4 +1,4 @@
-import { AclActionType } from './acl';
+import {AclActionType} from './acl';
 
 export enum NotificationPriority {
   HIGH = 1,
@@ -43,13 +43,58 @@ export enum NotificationType {
   COMPANY_SUSPEND_BO_SA,
   BACKGROUND_TASK,
   BALANCE_ALERT,
-  BULK_DONE ,
+  BULK_DONE,
   BULK_FINISHED,
   WEBHOOK,
   VNA_SUMMARY_CHECK_IN_REPORT,
   CONVERSATION_MESSAGE,
   CONVERSATION_LIVE_AGENT,
   TICKET_ASSIGN
+}
+
+export const mappingNotificationMessageToType = (messageEvent: MessageEvent): NotificationType => {
+  switch (messageEvent) {
+    case MessageEvent.BACKGROUND_TASK:
+      return NotificationType.BACKGROUND_TASK;
+    case MessageEvent.BALANCE_ALERT:
+      return NotificationType.BALANCE_ALERT;
+    case MessageEvent.CONVERSATION_MESSAGE:
+      return NotificationType.CONVERSATION_MESSAGE;
+    case MessageEvent.CONVERSATION_LIVE_AGENT:
+      return NotificationType.CONVERSATION_LIVE_AGENT;
+    case MessageEvent.BULK_DONE:
+      return NotificationType.BULK_DONE;
+    case MessageEvent.BULK_FINISHED:
+      return NotificationType.BULK_FINISHED;
+    case MessageEvent.CHANNEL_REQUEST:
+      return NotificationType.CHANNEL_REQUEST;
+    case MessageEvent.CHANNEL_OA_STATUS:
+      return NotificationType.CHANNEL_OA_STATUS;
+    case MessageEvent.TEMPLATE_APPROVE:
+      return NotificationType.TEMPLATE_APPROVE;
+    case MessageEvent.TEMPLATE_DELETE:
+      return NotificationType.TEMPLATE_DELETE;
+    case MessageEvent.TEMPLATE_REJECT:
+      return NotificationType.TEMPLATE_REJECT;
+    case MessageEvent.TEMPLATE_REQUEST:
+      return NotificationType.TEMPLATE_REQUEST;
+    case MessageEvent.WEBHOOK:
+      return NotificationType.WEBHOOK;
+    case MessageEvent.VNA_SUMMARY_CHECK_IN_REPORT:
+      return NotificationType.VNA_SUMMARY_CHECK_IN_REPORT;
+    case MessageEvent.TICKET_ASSIGN:
+      return NotificationType.TICKET_ASSIGN
+    case MessageEvent.COMPANY_KYC_REQUEST:
+      return NotificationType.COMPANY_KYC_REQUEST;
+    case MessageEvent.COMPANY_SUSPEND:
+      return NotificationType.COMPANY_SUSPEND;
+    case MessageEvent.COMPANY_KYC_APPROVE:
+      return NotificationType.COMPANY_KYC_APPROVE;
+    case MessageEvent.COMPANY_KYC_REJECT:
+      return NotificationType.COMPANY_KYC_REJECT;
+    case MessageEvent.COMPANY_SUSPEND_BO_SA:
+      return NotificationType.COMPANY_SUSPEND_BO_SA
+  }
 }
 
 export interface ReceiverPermission {
