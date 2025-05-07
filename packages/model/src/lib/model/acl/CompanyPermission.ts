95b0db6f3765ea571de491296c6cab0459807558
HPHoa <haphuochoa98@gmail.com>
2025-05-07 14:45:57 +0700

    Update acl

--- packages/model/src/lib/model/acl/CompanyPermission.ts
+++ packages/model/src/lib/model/acl/CompanyPermission.ts
@@ -162,5 +162,6 @@ export const LIST_MASTER_COMPANY_PERMISSION = [
   /* CHAT */
   { permission: PERMISSION.CHAT.READ, type: AclActionType.FULL },
   { permission: PERMISSION.CHAT.LIVE_AGENT, type: AclActionType.FULL },
-  { permission: PERMISSION.CHAT.DASHBOARD, type: AclActionType.FULL }
+  { permission: PERMISSION.CHAT.DASHBOARD, type: AclActionType.FULL },
+  { permission: PERMISSION.MONITOR.BULK, type: AclActionType.FULL }
 ];
