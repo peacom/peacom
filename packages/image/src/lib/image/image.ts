68f136c34bba41b5af360e1b30bab6a70a928035
Le Phuoc Canh <lephuoccanh@gmail.com>
2025-06-04 10:37:00 +0700

    Add image library.

--- packages/image/src/lib/image/image.ts
+++ packages/image/src/lib/image/image.ts
@@ -15,8 +15,14 @@ export interface GenerateItem {
 export interface GenerateImageProp {
   baseImage: string
   items: Array<GenerateItem>
+  output: string
 }
 
+/**
+ * baseImage: Image using for render all item
+ * output: Full path of output file
+ * @param props
+ */
 export function generateImage(props: GenerateImageProp): string {
   return 'image';
 }
