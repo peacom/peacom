60b924c7fe788f70ceec6de831e4ac17b836efdb
duynt <tanduy899@gmail.com>
2025-06-21 09:02:35 +0700

    - update

--- packages/model/src/lib/util/system.spec.ts
+++ packages/model/src/lib/util/system.spec.ts
@@ -1,8 +1,13 @@
 import {getHostName} from "./system.util";
+import { toLowerCaseNonAccentVietnamese, toNonAccentVietnamese } from '@peacom/model';
 
 describe('system.util', () => {
 
   it("getHostName", () => {
     console.log(getHostName())
+  })
+  it("toLowerCaseNonAccentVietnamese", () => {
+    console.log(toLowerCaseNonAccentVietnamese("a"));
+    console.log(toNonAccentVietnamese("à"));
   })
 });
