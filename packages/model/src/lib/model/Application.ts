669139de24b894e2475001a531c8ef346bee0b9a
huynhminhquan <quanhuynhminh1407@gmail.com>
2025-06-26 09:40:09 +0700

    update partner cakra_sms

--- packages/model/src/lib/model/Application.ts
+++ packages/model/src/lib/model/Application.ts
@@ -96,7 +96,8 @@ export enum PARTNER {
   MMP_MULTI_ROUTE,
   YULORE,
   VIET_GUY_SMPP,
-  TANLA
+  TANLA,
+  CAKRA
 }
 
 export const partnerStr = (id: PARTNER | null) => {
@@ -163,6 +164,8 @@ export const partnerStr = (id: PARTNER | null) => {
       return 'VIET GUY SMPP'
     case PARTNER.TANLA:
       return 'TANLA'
+    case PARTNER.CAKRA:
+      return 'CAKRA'
     default:
       return '';
   }
