286b14e6d76461f4d7de99a9a9bc14e7cc120aa1
Le Van Chien <levanchien95.cit@gmail.com>
2025-08-12 16:20:40 +0700

    Render data for alibaba button

--- packages/model/src/lib/util/template/template.util.ts
+++ packages/model/src/lib/util/template/template.util.ts
@@ -5,10 +5,10 @@ import {
   WHATSAPP_TEMPLATE_BUTTON_TYPE,
   WhatsappTemplateButton,
 } from '../../model/message';
-import {hasText, renderTemplate} from '../string.util';
-import {Url, URL_GENERATE_TYPE, URL_TYPE} from '../../model/url/Url';
-import {objectDeepClone} from '../general.util';
-import {DATE_TIME_FORMAT, parseDateTimeByFormat} from '../date';
+import { hasText, renderTemplate } from '../string.util';
+import { Url, URL_GENERATE_TYPE, URL_TYPE } from '../../model/url/Url';
+import { objectDeepClone } from '../general.util';
+import { DATE_TIME_FORMAT, parseDateTimeByFormat } from '../date';
 
 export interface GenerateUrlInput {
   redirectUrl: string;
@@ -84,7 +84,7 @@ async function renderWhatsappButton(
       }
     }
   */
-  const {url, redirectUrlType, landingPage} = button;
+  const { url, redirectUrlType, landingPage } = button;
   let generateResult;
   if (redirectUrlType === URL_TYPE.LANDING_PAGE) {
     generateResult = await generateUrl({
@@ -152,7 +152,7 @@ async function renderWhatsappAlibabaParams(
         (params.type === 'URL' || params.type === 'DYNAMIC_URL') &&
         params.isTracking
       ) {
-        const {url, data} = params;
+        const { url, data } = params;
         let redirectUrl = `${url}`;
         if (params.urlType === WHATSAPP_BUTTON_URL_TYPE.DYNAMIC) {
           redirectUrl = renderTemplate(`${url}${data}`, answerKeys);
@@ -206,13 +206,17 @@ async function renderAlibabaButton(
         button['flow_action_data']
       );
     }
+
+    // modify button
+    button.data = renderTemplate(button.data || '', answerKeys);
+
     return {
-      data: renderTemplate(button.data || '', answerKeys),
+      data: button.data,
       extraData,
     };
   }
 
-  const {url, redirectUrlType, landingPage} = button;
+  const { url, redirectUrlType, landingPage } = button;
   let generateResult = null;
   if (redirectUrlType === URL_TYPE.LANDING_PAGE) {
     generateResult = await generateUrl({
@@ -256,11 +260,11 @@ async function renderWhatsappAlibabaParamsV2(
   );
   console.log(`Render alibaba V2 answerKeys ~> `, JSON.stringify(answerKeys));
   const result: any = {};
-  const {header, body, buttons, carousel} = waParams;
+  const { header, body, buttons, carousel } = waParams;
 
   if (Array.isArray(header) && header.length) {
     for (const value of header) {
-      const {alibaba_param_name} = value;
+      const { alibaba_param_name } = value;
       // header text
       if (value.type === 'TEXT') {
         result[alibaba_param_name] = renderTemplate(value.data, answerKeys);
@@ -286,7 +290,7 @@ async function renderWhatsappAlibabaParamsV2(
   // body
   if (Array.isArray(body) && body.length) {
     for (const value of body) {
-      const {alibaba_param_name} = value;
+      const { alibaba_param_name } = value;
       result[alibaba_param_name] = renderTemplate(value.data, answerKeys);
     }
   }
@@ -294,8 +298,8 @@ async function renderWhatsappAlibabaParamsV2(
   // buttons
   if (Array.isArray(buttons) && buttons.length) {
     for (const button of buttons) {
-      const {alibaba_param_name} = button;
-      const {data, extraData} = await renderAlibabaButton(
+      const { alibaba_param_name } = button;
+      const { data, extraData } = await renderAlibabaButton(
         button,
         answerKeys,
         urls,
@@ -336,8 +340,8 @@ async function renderWhatsappAlibabaParamsV2(
       }
       if (Array.isArray(card.buttons) && card.buttons.length) {
         for (const button of card.buttons) {
-          const {alibaba_param_name} = button;
-          const {data, extraData} = await renderAlibabaButton(
+          const { alibaba_param_name } = button;
+          const { data, extraData } = await renderAlibabaButton(
             button,
             answerKeys,
             urls,
@@ -381,44 +385,50 @@ export interface RenderTemplateMessageResult {
   urls: Array<Url>;
 }
 
-export const parseMessageAndShortLink = async (message: string, generateUrl: generateUrlFunction) => {
+export const parseMessageAndShortLink = async (
+  message: string,
+  generateUrl: generateUrlFunction
+) => {
   let rs = message;
   const urlRegex = /(((https?:\/\/)|(www\.))\S+)/g;
   const cts = message.matchAll(urlRegex);
-  const listUrlSet = new Set<string>()
+  const listUrlSet = new Set<string>();
   for (let i of cts) {
-    listUrlSet.add(i[0])
+    listUrlSet.add(i[0]);
   }
   const listUrl: Array<string> = [...listUrlSet];
-  const urls = []
+  const urls = [];
   if (listUrl.length) {
-
     for (let i = 0; i < listUrl.length; i += 1) {
-      const t = listUrl[i]
+      const t = listUrl[i];
       const urlObj = await generateUrl({
-        urlOrigin: t, redirectUrl: t, content: {}, type: URL_TYPE.ORIGIN, generateType: URL_GENERATE_TYPE.REDIRECT
+        urlOrigin: t,
+        redirectUrl: t,
+        content: {},
+        type: URL_TYPE.ORIGIN,
+        generateType: URL_GENERATE_TYPE.REDIRECT,
       });
-      rs = rs.replaceAll(t, urlObj.link)
-      urls.push(urlObj.url)
+      rs = rs.replaceAll(t, urlObj.link);
+      urls.push(urlObj.url);
     }
   }
-  return {message: rs, urls};
-}
+  return { message: rs, urls };
+};
 
 export async function renderTemplateMessage({
-                                              content: _content,
-                                              answerKeys,
-                                              timezone,
-                                              generateUrl,
-                                            }: RenderTemplateMessageProp) {
+  content: _content,
+  answerKeys,
+  timezone,
+  generateUrl,
+}: RenderTemplateMessageProp) {
   const content = objectDeepClone(_content);
-  const rs = {content: null, urls: []} as RenderTemplateMessageResult;
+  const rs = { content: null, urls: [] } as RenderTemplateMessageResult;
   if (content) {
     if (content.peacomTemplateMessage) {
       content.peacomTemplateMessage.params = answerKeys;
     } else if (content.whatsappTemplateParam) {
       // WHATSAPP
-      const {header, body, buttons, media, alibabaParams, carousel} =
+      const { header, body, buttons, media, alibabaParams, carousel } =
         content.whatsappTemplateParam;
       // only Alibaba
       if (alibabaParams) {
@@ -525,7 +535,7 @@ export async function renderTemplateMessage({
       }
     } else if (content.zaloZnsTemplateParam) {
       // Zalo ZNS
-      const {templateData} = content.zaloZnsTemplateParam;
+      const { templateData } = content.zaloZnsTemplateParam;
       if (Array.isArray(templateData)) {
         content.zaloZnsTemplateParam.templateData = templateData.map(
           (item) => ({
@@ -538,20 +548,23 @@ export async function renderTemplateMessage({
       MESSAGE_TYPE.TEXT === content.type ||
       MESSAGE_TYPE.QUICK_REPLY === content.type
     ) {
-      const {previewUrl, message, shortLink} = content;
+      const { previewUrl, message, shortLink } = content;
       content.message = renderTemplate(message, answerKeys);
       if (shortLink) {
-        const {message, urls} = await parseMessageAndShortLink(content.message, generateUrl)
+        const { message, urls } = await parseMessageAndShortLink(
+          content.message,
+          generateUrl
+        );
         content.message = message;
-        rs.urls.push(...urls)
+        rs.urls.push(...urls);
       }
 
       // TODO: Implement insert link to content message after generate link
       if (previewUrl) {
-        const {image, title, redirectUrl, position} = previewUrl;
+        const { image, title, redirectUrl, position } = previewUrl;
         const generateUrlRs = await generateUrl({
           redirectUrl,
-          content: {image, title},
+          content: { image, title },
           type: URL_TYPE.ORIGIN,
           generateType: URL_GENERATE_TYPE.PREVIEW_URL,
           urlOrigin: redirectUrl,
@@ -738,9 +751,9 @@ export async function renderTemplateMessage({
       ].includes(content.type)
     ) {
       if (content.zaloTemplateMessage) {
-        const {elements, buttons} = content.zaloTemplateMessage;
+        const { elements, buttons } = content.zaloTemplateMessage;
         if (elements) {
-          const {header, text, table} = elements;
+          const { header, text, table } = elements;
           if (header && hasText(header.content)) {
             header.content = renderTemplate(header.content, answerKeys);
           }
