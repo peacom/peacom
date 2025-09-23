3205db67ba858cccdd69f7c07213cd82e961273f
Le Phuoc Canh <lephuoccanh@gmail.com>
2025-09-23 14:16:00 +0700

    Add customer attribute

--- packages/model/src/lib/model/crm/Customer.ts
+++ packages/model/src/lib/model/crm/Customer.ts
@@ -1,3 +1,5 @@
+import {TYPE_PROPERTY} from "../Property";
+
 export enum CUSTOMER_GENDER {
   MALE = 1,
   FEMALE = 2,
@@ -22,3 +24,73 @@ export enum CUSTOMER_ATTRIBUTE_PROPERTY_ID {
   APPLICATION = -9,
   CHANNEL = -10
 }
+
+export const CUSTOMER_PROPERTY_LABEL_BLACK_LIST = [
+  'gender', 'age', 'birthday', 'channel', 'label', 'email', 'phone', 'country', 'application'
+]
+
+export const CUSTOMER_PROPERTY_DEFAULT_LIST = [
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.GENDER, name: 'Gender',
+    priority: 0,
+    label: 'gender',
+    type: TYPE_PROPERTY.NUMBER,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.BIRTHDAY, name: 'Birthday',
+    priority: 0,
+    label: 'birthday',
+    type: TYPE_PROPERTY.DATE,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.EMAIL, name: 'Email',
+    priority: 0,
+    label: 'email',
+    type: TYPE_PROPERTY.TEXT,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.PHONE, name: 'Phone',
+    priority: 0,
+    label: 'phone',
+    type: TYPE_PROPERTY.TEXT,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.COUNTRY, name: 'Country',
+    priority: 0,
+    label: 'country',
+    type: TYPE_PROPERTY.NUMBER,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.LABEL, name: 'Label',
+    priority: 0,
+    label: 'label',
+    type: TYPE_PROPERTY.SELECT,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.NAME, name: 'Name',
+    priority: 0,
+    label: 'name',
+    type: TYPE_PROPERTY.TEXT,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.APPLICATION, name: 'Application',
+    priority: 0,
+    label: 'application',
+    type: TYPE_PROPERTY.NUMBER,
+    setting: null
+  },
+  {
+    id: CUSTOMER_ATTRIBUTE_PROPERTY_ID.CHANNEL, name: 'Channel',
+    priority: 0,
+    label: 'channel',
+    type: TYPE_PROPERTY.NUMBER,
+    setting: null
+  }
+]
