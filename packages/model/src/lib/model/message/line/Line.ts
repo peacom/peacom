998b59929e3049c9cafe9eda986dac1f50da5848
Le Van Chien <levanchien95.cit@gmail.com>
2026-02-11 08:56:03 +0700

    Add Line template type definition

---
+++ packages/model/src/lib/model/message/line/Line.ts
@@ -0,0 +1,9 @@
+/**
+ * @links https://developers.line.biz/en/reference/messaging-api/#template-messages
+ */
+export enum LineTemplateType {
+  BUTTONS = 'buttons',
+  CONFIRM = 'confirm',
+  CAROUSEL = 'carousel',
+  IMAGE_CAROUSEL = 'image_carousel',
+}
