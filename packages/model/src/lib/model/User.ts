efbcfce0c7ba9f0a5ccfe3d36639a8e96ab8233c
Tran Duc <minhduc07042004@gmail.com>
2026-05-04 21:02:20 +0700

    add USER_GLOBAL_STATUS and change USER_STATUS -> USER_COMPANY_STATUS

--- packages/model/src/lib/model/User.ts
+++ packages/model/src/lib/model/User.ts
@@ -1,4 +1,4 @@
-export enum USER_STATUS {
+export enum USER_COMPANY_STATUS {
   ACTIVE = 1,
   BLOCKED = 2,
   INVITED = 3,
@@ -12,3 +12,7 @@ export enum USER_INVITE_STATUS {
 }
 
 
+export enum USER_GLOBAL_STATUS {
+  ACTIVE = 1,
+  BLOCKED = 2,
+}
